122b93781adea6462e1f42e576bb1af6409e44cc
DSM3 <fatec-dsm3@academico.intra>
2026-02-25 22:42:58 -0300

    aula3.1

--- aula01-criando-a-api/routes/gameroutes.js
+++ aula01-criando-a-api/routes/gameroutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
+import gamecontroller from '../controllers/gamecontroller.js';
 const gameroutes = express.Router()
 
 // na camada de routes é armazenados os endpoints (urls) da api
 
 //endpoint para listar todos os games
-gameroutes.get("/games", gamescontroller.getALLgames)
+gameroutes.get("/games", gamecontroller.getALLgames)
 
 export default gameroutes;
